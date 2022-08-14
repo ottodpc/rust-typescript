b44a09965437e5597d418ae6764aa854bb9a8752
ottodpc <cyprienotto@outlook.fr>
2022-08-15 01:52:13 +0200

    Var & struct lifetime

--- main.rs
+++ main.rs
@@ -3,13 +3,16 @@ use std::fs::File;
 
 /// `main` is a function that is called when the program is executed.
 fn main() {
-    // NOTE: panic
-    // panic!("PANIC ! CRASH PROGRAMME");
+    let a: u8 = 48;
+    let b: u8 = 32;
+    println!("{}", superior(&a, &b))
+}
 
-    // unwrap
-    let file = File::open("./README.md")
-    // unwrap
-    // .unwrap();
-    .expect("Impossible de lire le ficher");
-    print!("{:?}", file);
+// 'a = copy la durée de vie car une fois scope est fini les vars s'efface avec lui
+fn superior<'a>(v1: &'a u8, v2: &'a u8) -> &'a u8 {
+    if v1 > v2 {
+        v1
+    } else {
+        v2
+    }
 }
