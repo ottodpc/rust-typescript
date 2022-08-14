5413943c4c249071eafe8fcca9a2183ed516c631
ottodpc <cyprienotto@outlook.fr>
2022-08-14 20:36:21 +0200

    CONSTANTE_VARIABLE

--- main.rs
+++ main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let mut mutable_variable = 78;
-    mutable_variable = 23;
-    println!("{}", mutable_variable);
-    
+    // CONSTANTE_VARIABLE vs let_variable: n'utilise pas la mémoire
+    const CONSTANTE_VARIABLE: i32 = 44;
+    let let_variable: i32 = 45;
+    println!("{}", CONSTANTE_VARIABLE);
 }
  
