89efb7b979019af346a79d9c82102c308c2a95a3
ottodpc <cyprienotto@outlook.fr>
2022-08-14 20:33:57 +0200

    mutable var

--- main.rs
+++ main.rs
@@ -1,11 +1,7 @@
 fn main() {
-    // entier {u: non signé positif, i: entier signé peut-être négatif}:
-    // u8: 1 octet en mémoire
-    // u32: 4 octet ...
-    // u64: 8 octet ..
-    // u128: 16 octet ...
-    // float: f32 , f64
-    let numerique_variable: i32 = 82;
-    println!("numerique_variable = {}", numerique_variable);
+    let mut mutable_variable = 78;
+    mutable_variable = 23;
+    println!("{}", mutable_variable);
+    
 }
  
