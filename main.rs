92993517f77b45dfa17af3404f51bf1fd9999c67
ottodpc <cyprienotto@outlook.fr>
2022-08-14 20:22:32 +0200

    number type

--- main.rs
+++ main.rs
@@ -1,4 +1,11 @@
 fn main() {
-    let message = "hello Rust";
-    println!("{}",message);
+    // entier {u: non signé positif, i: entier signé peut-être négatif}:
+    // u8: 1 octet en mémoire
+    // u32: 4 octet ...
+    // u64: 8 octet ..
+    // u128: 16 octet ...
+    // float: f32 , f64
+    let numerique_variable: i32 = 82;
+    println!("numerique_variable = {}", numerique_variable);
 }
+ 
