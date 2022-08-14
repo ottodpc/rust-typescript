2b5b5aa8433304160e8b6008759b00cab2af7e4f
ottodpc <cyprienotto@outlook.fr>
2022-08-14 21:49:44 +0200

    match & pattern binding

--- main.rs
+++ main.rs
@@ -1,19 +1,21 @@
 fn main() {
-    // ERROR: borrow of moved value: `c_var`
-    // Chaine de character est stocké dans le heap et le stack stock son pointeur
-    // il ne peut y avoir un seul pointeur (address). #Move #Ownership
-    let c_var = "c variable".to_owned();
-    // let b_var = c_var;
-    // println!("{} {}", c_var, b_var);
-
-    // Scope peut-être créer n'importe ou
-    {
-        // si borrow dans le scope avec c_var, c_var n'existera plus
+    let a = get_a();
+    // match & pattern binding avec tuple & b n'est pas stocké memoire
+    let ddd = ("a", 22, ("e", 78));
+    match ddd {
+        (a, _, (mut c, d)) if a == "e" => println!("{}", c),
+        _ => print!("default")
     }
-    
-    // Résolution: mais c_var n'existera plus
-    let mut d_var = c_var.clone();
-    d_var.push_str("mutable moved value");
-    println!("{}, {}", d_var, c_var);
 }
  
+fn get_a() {
+    let a: i32 = 34;
+    if  a > 0 {
+        // return a;
+        "a > 12";
+    } else {
+        "a < 12";
+    }
+}
+
+// match ~= de Switch case
