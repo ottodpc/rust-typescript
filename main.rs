fed012624c50037f798922be3ff8481949bc4953
ottodpc <cyprienotto@outlook.fr>
2022-08-14 20:38:41 +0200

    casting & suffix

--- main.rs
+++ main.rs
@@ -1,7 +1,7 @@
 fn main() {
     // CONSTANTE_VARIABLE vs let_variable: n'utilise pas la mémoire
     const CONSTANTE_VARIABLE: i32 = 44;
-    let let_variable: i32 = 45;
+    let let_variable_with_suffix_and_casting = 45u16 as i32;
     println!("{}", CONSTANTE_VARIABLE);
 }
  
